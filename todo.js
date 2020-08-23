bca3d639ea47498a98b3ea111493a28c1e286550
Zekun <ge52vuj@mytum.de>
2020-08-23 19:57:37 +0200

    implmented basic functionalities

--- todo.js
+++ todo.js
@@ -7,22 +7,23 @@ function insert(){
         return;
     }
 
+    let table1 = document.getElementById("table1");
+    let row = table1.insertRow(2);
+    let cell0 = row.insertCell(0); 
+    let cell1 = row.insertCell(1);
+    let cell2 = row.insertCell(2);
+
     let tick = document.createElement("INPUT");
     tick.type = "checkbox";
+    tick.onclick = tickOff;
 
-    let removeButton = document.createElement("BUTTON");
-    removeButton.innerHTML = "Delete";
-    removeButton.onclick = removeTask;
+    let deleteButton = document.createElement("BUTTON");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = deleteTask;
 
-    let table = document.getElementById("table");
-    let row = table.insertRow(2);
-    let cell1 = row.insertCell(0); 
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-
-    cell1.innerHTML = inputNewTask.value;
-    cell2.appendChild(tick);
-    cell3.appendChild(removeButton);
+    cell0.innerHTML = inputNewTask.value;
+    cell1.appendChild(tick);
+    cell2.appendChild(deleteButton);
 
     inputNewTask.value = '';
 }
@@ -36,7 +37,47 @@ document.getElementById("new-task").addEventListener("keyup", function(event){
 });
 
 
-function removeTask(){
-    let table = document.getElementById("table");
-    table.deleteRow(this.parentNode.parentNode.rowIndex);
+function deleteTask(){
+    let rowIndex = this.parentNode.parentNode.rowIndex;
+    let table = this.parentNode.parentNode.parentNode.parentNode;
+    table.deleteRow(rowIndex);
+}
+
+function tickOff(){
+    let table1 = document.getElementById("table1");
+    let table2 = document.getElementById("table2");
+    let rowIndex = this.parentNode.parentNode.rowIndex;
+    table1.deleteRow(rowIndex);
+
+    let row = table2.insertRow(1);
+
+    let cell0 = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    let cell2 = row.insertCell(2);
+
+    let deleteButton = document.createElement("BUTTON");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = deleteTask;
+
+    let tick = document.createElement("INPUT");
+    tick.id = "tick2";
+    tick.type = "checkbox";
+    tick.checked = true;
+    tick.onclick = tickBack;
+
+    cell0.innerHTML = this.parentNode.parentNode.cells[0].innerHTML;
+    cell1.appendChild(tick);
+    cell2.appendChild(deleteButton);
+}
+
+function tickBack(){
+    let table1 = document.getElementById("table1");
+    let table2 = document.getElementById("table2");
+    let rowIndex = this.parentNode.parentNode.rowIndex;
+    let content = table2.rows[rowIndex].cells[0].innerHTML;
+
+    document.getElementById("new-task").value = content;
+    document.getElementById("add").onclick();
+
+    table2.deleteRow(rowIndex);
 }
