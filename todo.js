559997834886920c85bd9020286833040ab6c54e
Zekun <ge52vuj@mytum.de>
2020-09-05 15:25:48 +0200

    the tasks are now added and saved through objects and not raw html

--- todo.js
+++ todo.js
@@ -1,60 +1,27 @@
 // localStorage.clear();   
 // console.log(document.getElementById("html").innerHTML);
-if (localStorage.getItem("changed") == "true"){
-    document.getElementById("html").innerHTML = localStorage.getItem("page");
-}
+
 // console.log(document.getElementById("html").innerHTML);
-function insert(){
-    if (document.getElementById("new-task").value == ''){
-        alert("Task is empty!");
-        return;
-    }
+let undoneMap = new Map([]);
+let doneMap = new Map([]);
 
-    let table1 = document.getElementById("table1");
-    table1.innerHTML = "<div class='task-div' onmouseenter='pressed()' onmouseleave='unpressed()'><i class='fas fa-caret-down'></i><p class='text'>" + document.getElementById('new-task').value + 
-    "</p><i id='tick' class='fas fa-check unchecked' onclick='tickOfftable1()'></i><i class='fas fa-trash-alt' onclick='deleteTask()'></i></div>"
-     + table1.innerHTML;
-    
-    // let inputNewTask = document.getElementById("new-task");
-
-    
-    // let table1 = document.getElementById("table1");
-    // let div = document.createElement("DIV");
-    // table1.insertBefore(div, table1.firstChild);
-    // let tick = document.createElement("I");
-    // let text = document.createElement("P");
-    // text.classList.add("text");
-    // text.innerHTML = document.getElementById("new-task").value;
-
-    // div.appendChild(text);
-    // tick.id = "tick"
-    // tick.classList.add("fas");
-    // tick.classList.add("fa-check");
-    // tick.classList.add("unchecked");
-    // div.appendChild(tick);
-    // tick.setAttribute("click", function() {
-    //     tickOfftable1(tick);}
-    // );
-    // // tick.addEventListener("click", 
-    // //     tickOfftable1
-    // // );
-    // console.log(tick);
-
-    // let trash = document.createElement("I");
-    // trash.classList.add("fas");
-    // trash.classList.add("fa-trash-alt");
-    // trash.onclick = deleteTask;
-
-    
-    // div.appendChild(tick);
-    // div.appendChild(trash);
-    // div.classList.add("task-div");
-
-    document.getElementById("new-task").value = '';
-    update();
-    localStorage.setItem("page", document.getElementById("html").innerHTML);
-    localStorage.setItem("changed", "true");
+if (localStorage.getItem("undoneMap")){
+    undoneMap = new Map(JSON.parse(localStorage.getItem("undoneMap")));
 }
+
+if (localStorage.getItem("doneMap")){
+    doneMap = new Map(JSON.parse(localStorage.getItem("doneMap")));
+}
+
+for (let value of undoneMap.values()) {
+    insert(value.name, value.id, false);
+}
+
+for (let value of doneMap.values()) {
+    insert(value.name, value.id, true);
+}
+
+table1 = document.getElementById("table1");
 
 
 document.getElementById("remove-all-undone").onclick = removeAll;
@@ -67,10 +34,142 @@ document.getElementById("new-task").addEventListener("keyup", function(event){
     }
 });
 
+function insert(name, id, done){
+    let table;
+    if (!done){
+        table = document.getElementById("table1");
+    }
+    else {
+        table = document.getElementById("table2");
+    }
+
+    let taskDiv = document.createElement("DIV");
+    let dropDown = document.createElement("I");
+    let tick = document.createElement("I");
+    let text = document.createElement("P");
+    let trash = document.createElement("I");
+
+    dropDown.classList.add("fas");
+    dropDown.classList.add("fa-caret-down");
+    tick.classList.add("fas");
+    tick.classList.add("fa-check");
+    if (!done)
+        tick.classList.add("unchecked");
+    else 
+        tick.classList.add("checked");
+    tick.onclick = tickOff;
+    text.classList.add("text");
+    text.innerHTML = name;
+    trash.classList.add("fas");
+    trash.classList.add("fa-trash-alt");
+    trash.onclick = deleteTask;
+
+    taskDiv.classList.add("task-div");
+    taskDiv.id = id;
+    taskDiv.appendChild(dropDown);
+    taskDiv.appendChild(text);
+    taskDiv.appendChild(tick);
+    taskDiv.appendChild(trash);
+
+    table.insertBefore(taskDiv, table.firstChild);
+
+    document.getElementById("new-task").value = '';
+    updateCount();
+}
+
+function saveAdd(){
+    if (document.getElementById("new-task").value == ''){
+            alert("Task is empty!");
+            return;
+    }
+
+    let taskName = document.getElementById("new-task").value;
+
+    const taskRep = {
+        id: Date.now(),
+        name: taskName,
+        done: false
+    };
+
+    insert(taskName, taskRep.id, false);
+
+    undoneMap.set(taskRep.id, taskRep);
+    localStorage.setItem("undoneMap", JSON.stringify(Array.from(undoneMap.entries())));
+    // console.log(undoneArray);
+}
+
+function deleteTask(){
+    let task = event.target.parentNode;
+    let table = task.parentNode;
+
+    if (table == table1){
+        table1.removeChild(task);
+        undoneMap.delete(parseInt(task.id));
+        localStorage.setItem("undoneMap", JSON.stringify(Array.from(undoneMap.entries())));
+        
+    }
+    else {
+        table2.removeChild(task);
+        doneMap.delete(parseInt(task.id));
+        localStorage.setItem("doneMap", JSON.stringify(Array.from(doneMap.entries())));
+    }
+    
+    
+    updateCount();
+}
+
+function tickOff(){
+    // alert("a");
+    let table1 = document.getElementById("table1");
+    let table2 = document.getElementById("table2");
+    let task = event.target.parentNode;
+    let taskRep;
+
+    if (task.parentNode == table1){
+        task.children[2].classList.remove("unchecked");
+        table1.removeChild(task);  
+        table2.insertBefore(task, table2.firstChild);
+        taskRep = undoneMap.get(parseInt(task.id));
+        undoneMap.delete(parseInt(task.id));
+        localStorage.setItem("undoneMap", JSON.stringify(Array.from(undoneMap.entries())));
+        doneMap.set(taskRep.id, taskRep);
+        localStorage.setItem("doneMap", JSON.stringify(Array.from(doneMap.entries())));
+    }
+    else{
+        task.children[2].classList.add("unchecked");
+        table2.removeChild(task);  
+        table1.insertBefore(task, table1.firstChild);
+        taskRep = doneMap.get(parseInt(task.id));
+        doneMap.delete(parseInt(task.id));
+        localStorage.setItem("doneMap", JSON.stringify(Array.from(doneMap.entries())));
+        undoneMap.set(taskRep.id, taskRep);
+        localStorage.setItem("undoneMap", JSON.stringify(Array.from(undoneMap.entries())));
+    }
+    updateCount();
+    
+}
+
+function dropDown(){
+    let caller = event.target;
+    if (caller.classList.contains("fa-check") || caller.classList.contains("fa-trash-alt")){
+        return;
+    }
+
+    if (caller.classList.contains("text") || caller.classList.contains("fa-caret-down")){
+        caller = caller.parentNode;
+    }
+    
+    // if (caller.nextSibling.classList.contains("not-drop-down")) {
+    //     caller.nextSibling.classList.remove("not-drop-down");
+    //     caller.nextSibling.classList.add("drop-down");
+    // }
+
+    // caller.nextSibling.classList.toggle("not-drop-down");
+}
+
+
 function pressed() {   
     let task = event.target;
-    console.log(task);
-
     task.style.borderTop = "solid";
     task.style.borderLeft = "solid";
     task.style.borderBottom = "none";
@@ -88,17 +187,15 @@ function unpressed() {
 
 }
 
-function save(){
-    localStorage.setItem("page", document.getElementById("html").innerHTML);
-    localStorage.setItem("changed", "true");
-    alert("a");
-}
+
 
 function removeAll() {
     if (!confirm("Are you sure?")){
         return;
     }
+
     let table;
+    
     if (this == document.getElementById("remove-all-undone")){
         table = document.getElementById("table1");
     }
@@ -111,60 +208,12 @@ function removeAll() {
     // console.log(table.firstChild);
 
     table.innerHTML = "";
-    update();
-    localStorage.setItem("page", document.getElementById("html").innerHTML);
-    localStorage.setItem("changed", "true");
-
-
-}
-
-
-function deleteTask(){
-    let task = event.target.parentNode;
-    let table = task.parentNode;
-
-    if (table == table1)
-        table1.removeChild(task);
-    else 
-        table2.removeChild(task);
-
-    update();
-    localStorage.setItem("page", document.getElementById("html").innerHTML);
-    localStorage.setItem("changed", "true");
-
-}
-
-function tickOfftable1(){
-    // alert("a");
-    let table1 = document.getElementById("table1");
-    let table2 = document.getElementById("table2");
-    let task = event.target.parentNode;
-
-    // a.parentNode.children[1].classList.remove("unchecked");
-    // console.log(a.parentNode);
-
-    // table1.removeChild(a.parentNode);  
-    // table2.insertBefore(a.parentNode, table2.firstChild);
-
-    if (task.parentNode == table1){
-        task.children[2].classList.remove("unchecked");
-        table1.removeChild(task);  
-        table2.insertBefore(task, table2.firstChild);
-    }
-    else{
-        task.children[2].classList.add("unchecked");
-
-        table2.removeChild(task);  
-        table1.insertBefore(task, table1.firstChild);
-    }
-
-    update();
+    updateCount();
     localStorage.setItem("page", document.getElementById("html").innerHTML);
     localStorage.setItem("changed", "true");
 }
 
-
-function update(){
+function updateCount(){
     document.getElementById("undone-count").innerHTML = table1.children.length;
     document.getElementById("finished-count").innerHTML = table2.children.length;
 }
